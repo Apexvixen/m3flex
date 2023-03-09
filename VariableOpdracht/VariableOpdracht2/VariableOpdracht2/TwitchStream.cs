35fa297df63354eddcc565eba7e4a4d0788865b5
ythiero <35019@ma-web.nl>
2023-03-09 14:15:25 +0100

    variabelen

---
+++ VariableOpdracht/VariableOpdracht2/VariableOpdracht2/TwitchStream.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace VariableOpdracht2
+{
+    internal class TwitchStream
+    {
+        //maak hier class variables aan:
+        // type=string, naam: name,
+        // type=string, naam: lastStreamName
+        // type=Game, naam: streamGame
+        // type=int, naam: followers
+        // type=bool, naam: automod
+        internal string name;
+        internal string laststreamname;
+        internal Game streamGame;
+        internal int cockFollowers;
+        internal bool balls;
+    }
+}
