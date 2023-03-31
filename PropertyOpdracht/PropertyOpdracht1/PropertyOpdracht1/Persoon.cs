8612d5fd601967d9eb04daae8cad0f525d34a8b6
ythiero <35019@ma-web.nl>
2023-03-31 10:45:36 +0200

    suicide

---
+++ PropertyOpdracht/PropertyOpdracht1/PropertyOpdracht1/Persoon.cs
@@ -0,0 +1,12 @@
+﻿namespace PropertyOpdracht1
+{
+    internal class Persoon
+    {
+        public Persoon()
+        {
+        }
+
+        public string Naam { get; internal set; }
+        public int Leeftijd { get; internal set; }
+    }
+}
