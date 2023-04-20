145fc8b4cf456bac31e99fbd7bd245ef44f7468b
ythiero <35019@ma-web.nl>
2023-04-20 11:57:10 +0200

    class&other

---
+++ Classes/Classes/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Classes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rect = new Rectangle();
+            rect.x = 100;
+            rect.y = 200;
+            rect.width = 50;
+            rect.height = 50;
+        }
+    }
+}
---
+++ hello world/hello world/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace hello_world
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string message = "hello world!";
+            {
+                foreach (char c in message)
+                {
+                    Console.BackgroundColor = ConsoleColor.Green;
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.Write(c);
+                    Console.ResetColor();
+                    
+                    System.Threading.Thread.Sleep(100);
+                }
+            }
+
+        }
+    }
+}
